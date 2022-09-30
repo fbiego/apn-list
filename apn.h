185ed2f5fe556f81183a681795a6cecaad3ccf6b
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-09-30 11:11:07 +0300

    Create apn.h

---
+++ apn.h
@@ -0,0 +1,19 @@
+
+
+typedef struct {
+  uint16_t mcc; //mcc code
+  uint16_t mnc; //mnc code
+  uint16_t code;  //country code
+  char* country;  //country name
+  char* iso;  //country iso code
+  char* network;  //network name
+  char* apn;  //apn name
+  char* user; //apn user name
+  char* pass; //apn password
+} Apn_Settings_t;
+
+Apn_Settings_t apn_list[] = {
+  
+  
+  
+}
