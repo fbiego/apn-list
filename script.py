4daeaa62107144ad5b57a324e6cb1c920c93496d
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-09-30 15:29:03 +0300

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ lines = open("apn.h", "r").readlines()
 x = 0
 c = 0
 i = 0
+j = 0
 lastC = ""
 count = 0
 apns = 0
@@ -21,6 +22,8 @@ for line in lines:
         
         if not ".apn=\"\"" in line:
             apns = apns + 1
+            if i == 0:
+                j = j + 1
             i = i + 1
         det = {"name": country, "start": c, "end": x, "count" : i}
         countries[iso] = {"name": country}
@@ -37,11 +40,10 @@ with open("README.md", "w") as f:
     f.write("List of apn settings\n\n")
     f.write("[![Script](https://github.com/fbiego/apn-list/actions/workflows/main.yml/badge.svg)](https://github.com/fbiego/apn-list/actions/workflows/main.yml)\n")
     f.write("## Status\n")
-    f.write("| Description | Count |\n")
-    f.write("| --- | --- |\n")
-    f.write("| Available | "+ str(apns) + " |\n")
-    f.write("| Total | "+ str(count) + " |\n")
-    f.write("| Countries | "+ str(len(countries))+ "|\n")
+    f.write("| Description | Available | Total |\n")
+    f.write("| --- | --- | --- |\n")
+    f.write("| APN Settings | "+ str(apns) + " | "+ str(count) + " |\n")
+    f.write("| Countries | " + str(j) + " | "+ str(len(countries))+ " |\n")
     f.write("\n")
     f.write("## Contributing\n")
     f.write("- Add apn settings on the existing list then modify the apn, user & pass or\n")
